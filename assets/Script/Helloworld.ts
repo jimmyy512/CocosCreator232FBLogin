2bea3dbd8664e90766fb221f0dcdd812c301e1cd
Majitoo <jimmyy5121@gmail.com>
2020-05-09 12:44:05 +0800

    init

--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -1,4 +1,4 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Helloworld extends cc.Component {
@@ -7,10 +7,34 @@ export default class Helloworld extends cc.Component {
     label: cc.Label = null;
 
     @property
-    text: string = 'hello';
+    text: string = 'hello224';
 
-    start () {
+    start() {
         // init logic
-        this.label.string = this.text;
+        console.log(this.text);
+        this.label.string = "start";
+
+        //fb登入初始化
+        sdkbox.PluginFacebook.init();
+
+        if (cc.sys.isNative) {
+            if (sdkbox.PluginFacebook.isLoggedIn())
+                sdkbox.PluginFacebook.logout();
+            sdkbox.PluginFacebook.login()
+            sdkbox.PluginFacebook.setListener({
+                onLogin: (isLogin, msg) => {
+                },
+                onAPI: (tag, data) => { },
+                onSharedSuccess: function (data) { },
+                onSharedFailed: function (data) { },
+                onSharedCancel: function () { },
+                onPermission: function (isLogin, msg) { },
+                onGetUserInfo: (
+                    (userInfo) => {
+                        this.label.string = sdkbox.PluginFacebook.getUserID()
+                    }
+                )
+            });
+        }
     }
 }
